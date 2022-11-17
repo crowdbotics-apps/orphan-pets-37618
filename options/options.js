068c3553044d7841f28c88e5665d272a5dfa039d
Crowdbotics <team@crowdbotics.com>
2022-11-17 02:18:20 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "orphan_pets_37618",
+  url: "https://orphan_pets_37618.botics.co",
+  api: "https://orphan_pets_37618.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
